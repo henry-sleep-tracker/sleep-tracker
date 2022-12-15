1260a4ef20f0ec120783bf40fe2d1f7bc60187b8
elhombrej <94816708+elhombrej@users.noreply.github.com>
2022-12-15 13:32:20 -0300

    joaquin

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ function App() {
         />
         <Route
           path="/home"
-          element={user?.email ? <Home user={user} /> : <Navigate to="/" />}
+          element={user?.email ? <Home user={user} /> : <Navigate to="/" />}//cambio
         />
       </Routes>
     </BrowserRouter>
