18cfc214463590fdea319755c967c62156d7f905
arza1012 <arzabala12@gmail.com>
2022-12-20 16:33:22 +0100

    Wip

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Collection from "./resume";
-import Graph from "./Dream-Graph";
+import Graph from "../Graphs/Dream-Graph";
 
 const Home = () => {
   let user = {
