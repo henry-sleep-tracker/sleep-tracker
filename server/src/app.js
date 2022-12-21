6bb173af60cf1ff71e8992a967cd53e8f5fac902
jfparrado <jnsparrado@hotmail.com>
2022-12-21 11:20:37 -0500

    refactor (server/src/app.js): se añade libreria para usar sesiones se configuran las sesiones se modifica el header para que al hacer deploy deje hacer requests

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const session = require("express-session"); //esto permite crear sesiones con un tiempo de expiracion y demas
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
+const cors = require("cors");
+const { SECRET } = process.env;
 
 require("./db.js");
 
@@ -13,9 +16,20 @@ server.name = "API";
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
+server.use(
+  session({
+    name: "sid",
+    secret: SECRET, //aca trae el secreto del archivo .env
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 2, // 2 horas en milisegundos
+    },
+  })
+);
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
