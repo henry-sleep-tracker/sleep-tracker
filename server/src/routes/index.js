a859b5d3b21ba0fd3b0e0179a2a7b8d912bbd682
arza1012 <arzabala12@gmail.com>
2022-12-21 20:10:51 +0100

    added index.js to routes

---
+++ server/src/routes/index.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+const restapiRouter = require("./restapi");
+const userMiddleware = require("./middlewares/user.js");
+const signupMiddleware = require("./middlewares/signup.js");
+const loginMiddleware = require("./middlewares/login.js");
+
+const router = Router();
+
+router.use("/restapi", restapiRouter);
+router.use("/user", userMiddleware);
+router.use("/signup", signupMiddleware);
+router.use("/login", loginMiddleware);
+
+module.exports = router;
