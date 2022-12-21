fbf247eb136b8b7ac1e90cfcb5dea900c1641172
arza1012 <arzabala12@gmail.com>
2022-12-21 18:10:01 +0100

    config fixes

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
 
@@ -10,8 +9,9 @@ const server = express();
 
 server.name = "API";
 
-server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-server.use(bodyParser.json({ limit: "50mb" }));
+server.use(express.urlencoded({ extended: false }));
+server.use(express.json());
+server.use(express.text());
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
@@ -19,7 +19,7 @@ server.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, ContentType, Content-Type, Accept"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,16 +1,14 @@
 const { Router } = require("express");
-
-const restapiRouter = require("./restapi");
-
-const router = Router();
 const userMiddleware = require("./middlewares/user.js");
 const signupMiddleware = require("./middlewares/signup.js");
 const loginMiddleware = require("./middlewares/login.js");
+const restapiRouter = require("./restapi");
+
+const router = Router();
 
 router.use("/user", userMiddleware);
 router.use("/signup", signupMiddleware);
 router.use("/login", loginMiddleware);
-
 router.use("/restapi", restapiRouter);
 
 module.exports = router;
