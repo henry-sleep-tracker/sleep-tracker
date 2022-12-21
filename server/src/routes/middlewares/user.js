cac54eea1eb3991d4b7136d8f464b2ba25d83f7a
jfparrado <jnsparrado@hotmail.com>
2022-12-21 11:22:17 -0500

    fix(server/routes/middlewares/user.js): ya deja postear usuarios

--- server/src/routes/middlewares/user.js
+++ server/src/routes/middlewares/user.js
@@ -14,10 +14,11 @@ router.post("/", async (req, res) => {
       nationality,
       birthday,
     } = req.body;
+    const hashedPassword = password;
     const bodyInfo = {
       googleId,
       email,
-      password,
+      hashedPassword,
       names,
       lastNames,
       nationality,
@@ -33,18 +34,19 @@ router.post("/", async (req, res) => {
     ) {
       return res.status(404).send("Falta enviar datos obligatorios");
     }
+    console.log("1");
     const isRepeated = await repeatedEmail(email);
-    if (strIds) {
+    console.log("2");
+    if (isRepeated.length > 0) {
       return res.status(406).send(`El email ya esta registrado en la BD`);
     }
+    console.log("3");
     let createdUser = await postUser(bodyInfo);
-
+    console.log("4");
     res.status(201).json(createdUser); //201 es que fue creado
   } catch (error) {
-    console.log("El error middleware activity post / es:", error.message);
-    res
-      .status(401)
-      .send("El error middleware activity post / es:", error.message);
+    console.log("El error middleware user post / es:", error.message);
+    res.status(401).send("El error middleware user post / es:", error.message);
   }
 });
 module.exports = router;
