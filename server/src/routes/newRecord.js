b564af1af453a7130864538d47ccd2cc1e1f8a5f
Karso23 <karsoreef@hotmail.com>
2022-12-21 16:04:11 -0600

    index router update

--- server/src/routes/newRecord.js
+++
@@ -1,5 +0,0 @@
-const { Router } = require("express");
-const router = Router();
-
-const newRecord = require("./newRecord_router"); // ==> Oscar Sarabia
-router.post("/newRecord", newRecord); // ==> Oscar Sarabia
