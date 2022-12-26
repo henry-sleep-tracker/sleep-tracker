924a8eddf30cd267237c8915dea3a616311e8b02
jfparrado <jnsparrado@hotmail.com>
2022-12-26 10:41:28 -0500

    feat(server/middlewares/user): correcto hasheo de la contraseña por parte del servidor se añade endpoint para obtener user basado en un email

--- server/src/middlewares/user.js
+++ server/src/middlewares/user.js
@@ -1,7 +1,11 @@
 const { Router } = require("express");
-const { Activity } = require("../db");
 const router = Router();
-const { postUser, repeatedEmail } = require("../controllers/user");
+const {
+  postUser,
+  repeatedEmail,
+  getUserByEmail,
+} = require("../controllers/user");
+const bcrypt = require("bcrypt");
 
 router.post("/", async (req, res) => {
   try {
@@ -13,8 +17,8 @@ router.post("/", async (req, res) => {
       lastNames,
       nationality,
       birthday,
-    } = req.body;
-    const hashedPassword = password;
+    } = req.body.user;
+    const hashedPassword = await bcrypt.hash(password, 10); //la segunda variable es el numero de rondas que se encriptara
     const bodyInfo = {
       googleId,
       email,
@@ -34,19 +38,32 @@ router.post("/", async (req, res) => {
     ) {
       return res.status(404).send("Falta enviar datos obligatorios");
     }
-    console.log("1");
     const isRepeated = await repeatedEmail(email);
-    console.log("2");
     if (isRepeated.length > 0) {
       return res.status(406).send(`El email ya esta registrado en la BD`);
     }
-    console.log("3");
     let createdUser = await postUser(bodyInfo);
-    console.log("4");
     res.status(201).json(createdUser); //201 es que fue creado
   } catch (error) {
     console.log("El error middleware user post / es:", error.message);
     res.status(401).send("El error middleware user post / es:", error.message);
+  }
+});
+
+router.get("/:email", async (req, res) => {
+  const { email } = req.params;
+  try {
+    const user = await getUserByEmail(email);
+    if (!email) {
+      return res.status(204).send("that id does not exist in the database");
+    } else {
+      return res.status(200).send(user);
+    }
+  } catch (error) {
+    console.log("El error middleware user get /:email es:", error.message);
+    res
+      .status(401)
+      .send("El error middleware user get /:email es:", error.message);
   }
 });
 module.exports = router;
