7c0592d3fd1c3d4729fbc84fbd423906204400b5
jfparrado <jnsparrado@hotmail.com>
2022-12-27 13:28:41 -0500

    refactor (server/app): se elimina la configiraciond e passport

--- server/src/app.js
+++ server/src/app.js
@@ -2,14 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const routes = require("./index.js");
-const session = require("express-session"); //esto permite crear sesiones con un tiempo de expiracion y demas. guarda el usuario a travez de varias paginas
-const initializePassport = require("./passport-config");
-const passport = require("/passport"); // permite usar muchas formas de loguearse
-const flash = require("/express-flash"); // muestra mensges en caso de que hayamos escrito algo mal al iniciar sesion
-const { SECRET } = process.env;
-initializePassport(passport, (email) =>
-  users.find((user) => user.email === email)
-);
+const session = require("express-session"); //esto permite crear sesiones con un tiempo de
 require("./db.js");
 const server = express();
 server.name = "API";
@@ -19,20 +12,6 @@ server.use(express.json());
 server.use(express.text());
 server.use(cookieParser());
 
-app.use(flash());
-server.use(
-  session({
-    name: "sid",
-    secret: SECRET || "contraseña", //aca trae el secreto del archivo .env
-    resave: false, //guarda nuestras variables de entorno si nada se cambia. decimos no
-    saveUninitialized: false, //queremos guardar un valor vacio en la sesion si no hay valor. decimos no
-    // cookie: {
-    //   maxAge: 1000 * 60 * 60 * 2, // 2 horas en milisegundos
-    // },
-  })
-);
-app.use(passport.initialize()); //una funcion basica de inicializacion
-app.use(passport.session()); //vamos a guardar nuestras variables a travez de toda la sesion del usuario, que va  funcionar con la configuracionde session de arriba
 server.use(morgan("dev"));
 server.use((req, res, next) => {
   //Choose the line below to test with local server
