9caf2a1195c164dc7c3a2fe84e317b3393822fa2
jfparrado <jnsparrado@hotmail.com>
2022-12-27 13:35:21 -0500

    refactor (client/components/register): se elimina console log inecesario se añade dependencia de estilos

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -3,11 +3,11 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { postUser,getUserByEmail } from "../../actions";
 import { useDispatch } from "react-redux";
+import {TextField,Typography} from "@mui/material";
 
 let nationalities = [
  'Afganistan','Albania','Alemania','Andorra','Angola','Antigua y Barbuda','Arabia Saudita','Argelia','Argentina','Armenia','Australia','Austria','Azerbaiyán','Bahamas','Bangladés','Barbados','Baréin','Bélgica','Belice','Bielorrusia','Benín','Birmania','Bolivia','Bosnia y Herzegovina','Botsuana','Brasil','Brunei','Bulgaria','Burkina Faso','Burundi','Bután','Cabo Verde','Camboya','Camerún','Canadá','Catar','Chad','República Checa','Chile','China','Chipre','Ciudad del Vaticano','Colombia','Comoras','Corea del Norte','Corea del Sur','Costa de Marfil','Costa Rica','Croacia','Cuba','Dinamarca','Dominica','República Dominicana','Ecuador','Egipto','El Salvador','Emiratos Árabes Unidos','Eritrea','Eslovaquia','Eslovenia','España','Estados Unidos','Estonia','Etiopía','Filipinas','Finlandia','Fiyi','Francia','Gabón','Gambia','Georgia','Ghana','Granada','Grecia','Guatemala','Guinea','Guinea-Bisáu','Guinea Ecuatorial','Guyana','Haití','Honduras','Hungría','India','Indonesia','Irak','Irán','Irlanda','Islandia','Israel','Italia','Jamaica','Japón','Jordania','Kazajistán','Kenia','Kirguistán','Kiribati','Kuwait','Laos','Lesoto','Letonia','Líbano','Liberia','Libia','Liechtenstein','Lituania','Luxemburgo','Macedonia del Norte','Madagascar','Malasia','Malaui','Maldivas','Mali','Malta','Marruecos','Islas Marshall','Mauricio','Mauritania','México','Micronesia','Moldavia','Mónaco','Mongolia','Montenegro','Mozambique','Namibia','Nauru','Nepal','Nicaragua','Níger','Nigeria','Noruega','Nueva Zelanda','Omán','Países Bajos','Pakistán','Palaos','Palestina','Panamá','Papúa Nueva Guinea','Paraguay','Perú','Polonia','Portugal','Reino Unido','República Centroafricana','República del Congo','República Democrática del Congo','Ruanda','Rumania','Rusia','Islas Salomón','Samoa','San Cristóbal y Nieves','San Marino','San Vicente y las Granadinas','Santa Lucía','Santo Tomé y Príncipe','Senegal','Serbia','Seychelles','Sierra Leona','Singapur','Siria','Somalia','Sri Lanka','Suazilandia','Sudáfrica','Sudán','Sudán del Sur','Suecia','Suiza','Surinam','Tailandia','Tanzania','Tayikistán','Timor Oriental','Togo','Tonga','Trinidad y Tobago','Túnez','Turkmenistán','Turquía','Tuvalu','Ucrania','Uganda','Uruguay','Uzbekistán','Vanuatu','Venezuela','Vietnam','Yemen','Yibuti','Zambia','Zimbabue'
 ];
-console.log(nationalities);
 let keyNationalities=0;
 let yourDate = new Date()
 yourDate=yourDate.toISOString().split('T')[0]
@@ -111,17 +111,19 @@ function validate(input){ //aca entra todo el estado input
 
   return (
     <form onSubmit={(event) => handleSubmit(event)}>
-      <h1>Registro{yourDate}</h1>
+      <Typography variant='h2'>Registro</Typography>
       <div>
         <label htmlFor="names">{`Nombre(s)*:`} </label>
-        <input
-          type="text"
-          name="names"
-          placeholder="Nombre(s)"
-          maxLength="50"
-          onChange={(event) => handleChange(event)}
-          required
-        />
+        <TextField id="outlined-basic" label="Outlined" variant="outlined"
+        type="text"
+        name="names"
+        placeholder="Nombre(s)"
+        maxLength="50"
+        onChange={(event) => handleChange(event)}
+        required />
+        {/* <input
+          
+        /> */}
       </div>
 
       <div>
