3abb2ace4a7c70ed49e5b76e0fa8989b47011969
jfparrado <jnsparrado@hotmail.com>
2022-12-29 14:34:29 -0500

    db

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const port = process.env.DB_PORT || 3001;
+const port = process.env.PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(port, () => {
