10bfe88b4c1bee7fa6da686d8c98c39086988985
jfparrado <jnsparrado@hotmail.com>
2023-01-03 11:52:41 -0500

    feat(server/middlewares/user): se añade ruta olvide contraseña se añade ruta resetear contraseña se cambia el nombre repeated email

--- server/src/middlewares/user.js
+++ server/src/middlewares/user.js
@@ -4,8 +4,13 @@ const {
   postUser,
   repeatedEmail,
   getUserByEmail,
+  getUserById,
+  updatePassword,
 } = require("../controllers/user");
 const bcrypt = require("bcrypt");
+const JWT_SECRET = "CVDF61651BV231TR894VBCX51LIK5LÑK84";
+const basicURL = "http://localhost:3000";
+const jwt = require("jsonwebtoken");
 
 router.post("/", async (req, res) => {
   try {
@@ -31,8 +36,8 @@ router.post("/", async (req, res) => {
     if (!email || !names || !lastNames) {
       return res.status(404).send("Falta enviar datos obligatorios");
     }
-    const isRepeated = await repeatedEmail(email);
-    if (isRepeated.length > 0) {
+    const oldUser = await repeatedEmail(email);
+    if (oldUser.length > 0) {
       return res.status(406).send(`El email ya esta registrado en la BD`);
     }
     let createdUser = await postUser(bodyInfo);
@@ -47,16 +52,59 @@ router.post("/google", async (req, res) => {
   const { email, names, lastNames } = req.body;
   const bodyInfo = { email, names, lastNames };
   try {
-    const isRepeated = await repeatedEmail(email);
-    console.log("email repetido:", isRepeated);
-    if (isRepeated.length > 0) {
-      return res.status(202).send(isRepeated);
+    const oldUser = await repeatedEmail(email);
+    if (oldUser.length > 0) {
+      return res.status(202).send(oldUser);
     }
     let createdUser = await postUser(bodyInfo);
     res.status(201).json(createdUser); //201 es que fue creado
   } catch (error) {
     console.log("El error middleware login post /manual es:", error.message);
     res.status(401).send("El error middleware login post /manual es");
+  }
+});
+
+router.post("/forgot-password", async (req, res) => {
+  const { email } = req.body;
+  console.log("recibi el email:", email);
+  try {
+    const oldUser = await getUserByEmail(email);
+    if (oldUser.id === 0) {
+      return res.status(202).send({ status: "el usuario no existe" });
+    }
+    const secret = JWT_SECRET + oldUser.hashedPassword;
+    const token = jwt.sign({ email: oldUser.email, id: oldUser.id }, secret, {
+      expiresIn: "50m",
+    });
+    const link = `${basicURL}/reiniciar_contrasena/${oldUser.id}/${token}`;
+    console.log("el link es:", link);
+    res.status(200).json(link); //201 es que fue creado
+  } catch (error) {
+    console.log(
+      "El error middleware user post /forgot-password es:",
+      error.message
+    );
+    res.status(401).send("El error middleware user post /forgot-password es");
+  }
+});
+
+router.post("/reset-password/:id/:token", async (req, res) => {
+  const { id, token } = req.params;
+  const { password } = req.body;
+
+  const oldUser = await getUserById(id);
+  if (!oldUser) {
+    return res.status(202).send("el usuario no existe");
+  }
+  const secret = JWT_SECRET + oldUser.hashedPassword;
+  try {
+    const verify = jwt.verify(token, secret);
+    console.log("verify:", verify);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    updatePassword(id, hashedPassword);
+    res.status(200).send("Verificado");
+  } catch (error) {
+    res.status(400).send("Sin verificar");
   }
 });
 
