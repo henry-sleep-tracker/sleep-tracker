4650302e3dc9409166f9909148d4fa693882227b
jfparrado <jnsparrado@hotmail.com>
2023-01-04 18:23:17 -0500

    feat(client/actions/authContext): Se añaden keys de user_id y de plan expiration_date

--- client/src/actions/authContext.js
+++ client/src/actions/authContext.js
@@ -2,6 +2,8 @@ import { createContext, useCallback, useContext } from "react";
 import PropTypes from "prop-types";
 import { useState, useMemo } from "react";
 const MY_AUTH_APP = "MY_AUTH_APP_1";
+const USER_ID = "USER_ID";
+const PLAN_EXPIRATION_DATE = "PLAN_EXPIRATION_DATE";
 
 export const AuthContext = createContext(); //es un objeto que adentro tiene un provider
 
@@ -11,22 +13,29 @@ export function AuthContextProvider({ children }) {
     window.localStorage.getItem(MY_AUTH_APP) ?? false
   ); //el localstorage permite guardar valores para que asi se cierre la pestaña guarde valores, entonces cada vez que abra la app se sabra si esta conectado o no
   const [userId, setUserId] = useState(
-    window.localStorage.getItem("USER_ID") ?? null
+    window.localStorage.getItem(USER_ID) ?? null
   );
+  const [planExpDate, setPlanExpDate] = useState(
+    window.localStorage.getItem(PLAN_EXPIRATION_DATE) ?? "1900-00-00"
+  );
+  const payPlan = useCallback(function (expDate) {
+    //esto lo usa el componente log in cuando se valide la contraseña y el email
+    window.localStorage.setItem(PLAN_EXPIRATION_DATE, expDate);
+    setPlanExpDate(expDate);
+  }, []);
   const login = useCallback(function (id) {
-    console.log("me llego el userId:", id);
-    debugger;
     //esto lo usa el componente log in cuando se valide la contraseña y el email
     window.localStorage.setItem(MY_AUTH_APP, true); //cuando se invoque establecera en el localstorage que establece el valor true para la clave my_auth_app
+    window.localStorage.setItem(USER_ID, id);
     setIsAuthenticated(true); //actualiza el estado
     setUserId(id);
-    console.log("userId:", userId);
-    debugger;
   }, []);
   const logout = useCallback(function () {
     //esto lo usa el componente log in cuando se valide la contraseña y el email
     window.localStorage.removeItem(MY_AUTH_APP); //cuando se invoque establecera en el localstorage que establece el valor true para la clave my_auth_app
+    window.localStorage.removeItem(USER_ID);
     setIsAuthenticated(false); //actualiza el estado
+    setUserId(null);
   }, []);
   const value = useMemo(
     () => ({
@@ -34,8 +43,10 @@ export function AuthContextProvider({ children }) {
       login,
       logout,
       isAuthenticated,
+      userId,
+      planExpDate,
     }),
-    [login, logout, isAuthenticated]
+    [login, logout, payPlan, isAuthenticated, userId, planExpDate]
   );
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
