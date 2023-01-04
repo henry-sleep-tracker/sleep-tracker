a6d9b7379e1c1b212d9a0faf82192746d7715f7f
arza1012 <82216404+arza1012@users.noreply.github.com>
2023-01-04 20:46:23 +0100

    fixes to stage-session association

--- server/src/db.js
+++ server/src/db.js
@@ -19,18 +19,18 @@ const modelDefiners = [];
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
 fs.readdirSync(path.join(__dirname, "/models"))
   .filter(
-    file =>
+    (file) =>
       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
   )
-  .forEach(file => {
+  .forEach((file) => {
     modelDefiners.push(require(path.join(__dirname, "/models", file)));
   });
 
 // Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach(model => model(sequelize));
+modelDefiners.forEach((model) => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map(entry => [
+let capsEntries = entries.map((entry) => [
   entry[0][0].toUpperCase() + entry[0].slice(1),
   entry[1],
 ]);
@@ -97,13 +97,8 @@ Plans.belongsTo(User, {
   timestamps: false,
 });
 
-// User.hasMany(Session);
-// Session.belongsTo(User);
-
-Session.hasMany(Stage);
-Stage.belongsTo(Session, {
-  through: "sessionId",
-});
+User.hasMany(Session);
+Session.belongsTo(User);
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
