48fea0fc0e4039c76843abb78d30b10feb313c8c
jfparrado <jnsparrado@hotmail.com>
2023-01-08 17:13:39 -0500

    feat(client/components/privateroute): se trae el estado de planexpdate

--- client/src/components/PrivateRoute/PrivateRoute.jsx
+++ client/src/components/PrivateRoute/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import {Navigate, Outlet} from "react-router-dom"
 import {useAuthContext} from "../../actions/authContext"
 
 export default function PublicRoute(){
-    const {isAuthenticated}= useAuthContext();//consume el contexto para saber si esta autenticado
+    const {isAuthenticated,planExpDate}= useAuthContext();//consume el contexto para saber si esta autenticado
     if (!isAuthenticated) { //si esta autenticado que vaya a la seccion privada
         return <Navigate to="/"/>
     }
