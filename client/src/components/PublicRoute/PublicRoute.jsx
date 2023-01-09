12cba4a640fb8265764384c47855107ca9655b52
jfparrado <jnsparrado@hotmail.com>
2023-01-08 19:18:32 -0500

    feat(client/components/publicRoutes): verifica cuando se le expira el plan y redirige basado en eso

--- client/src/components/PublicRoute/PublicRoute.jsx
+++ client/src/components/PublicRoute/PublicRoute.jsx
@@ -2,13 +2,19 @@ import {Navigate, Outlet} from "react-router-dom"
 import {useAuthContext} from "../../actions/authContext"
 
 export default function PublicRoute(){
-    const {isAuthenticated}= useAuthContext();//consume el contexto para saber si esta autenticado
-    if (isAuthenticated) { //si esta autenticado que vaya a la seccion privada
+    let yourDate = new Date()
+yourDate=yourDate.toISOString().split('T')[0]
+    const {isAuthenticated,planExpDate}= useAuthContext();//consume el contexto para saber si esta autenticado
+    if (isAuthenticated&&(planExpDate<yourDate)) { //si esta autenticado que vaya a la seccion privada
+        return <Navigate to="/private/planes"/>
+    }else if(isAuthenticated&&(planExpDate>=yourDate)){
         return <Navigate to="/private"/>
+    }else{
+        return(
+            <div>
+                 <Outlet/> {/*todo lo que esta anidado dentro de publico */}
+            </div>
+        )
     }
-    return(
-        <div>
-             <Outlet/> {/*todo lo que esta anidado dentro de publico */}
-        </div>
-    )
+    
 }
