03624f590b597f1953f8a4dc36b41329d4ab3de5
arza1012 <82216404+arza1012@users.noreply.github.com>
2023-01-10 19:58:52 +0100

    added endpoint to get steps from fitbit

--- server/src/routes/getSleepFitbit.js
+++ server/src/routes/getSleepFitbit.js
@@ -7,6 +7,7 @@ const { Op } = require("sequelize");
 router.post("/", async (req, res) => {
   try {
     const { code, userId } = req.body;
+    console.log("code", code);
 
     const response = await fetch(
       `https://api.fitbit.com/oauth2/token?client_id=238Z55&code=${code}&code_verifier=48601r4939480m540t0v3p6b3z3a4l323s0a6f0q0t4e1e0y133809063f1q2n425j362z0k5t1c1p0m6u2d4w3a6b4p2j4z2f175u182t5p6n576h2p3e2b5j090g68&grant_type=authorization_code`,
