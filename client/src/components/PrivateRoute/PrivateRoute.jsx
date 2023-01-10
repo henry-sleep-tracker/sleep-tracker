fb6ad136b2ed47a0c01b01e6f49f5af012336de7
jfparrado <jnsparrado@hotmail.com>
2023-01-10 14:45:10 -0500

    feat(client/components/privateroute): se añade el nav bar para todas las paginas privadas

--- client/src/components/PrivateRoute/PrivateRoute.jsx
+++ client/src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,14 +1,15 @@
 import {Navigate, Outlet} from "react-router-dom"
 import {useAuthContext} from "../../actions/authContext"
-
-export default function PublicRoute(){
-    const {isAuthenticated,planExpDate}= useAuthContext();//consume el contexto para saber si esta autenticado
+import ResponsiveAppBar from "../Home/Nav";
+export default function PrivateRoute(){
+    const {isAuthenticated}= useAuthContext();
     if (!isAuthenticated) { //si esta autenticado que vaya a la seccion privada
-        return <Navigate to="/"/>
+        return <Navigate to="/"/> //aca se quita el return
     }
     return(
         <div>
-             <Outlet/> {/*todo lo que esta anidado dentro de publico */}
+            <ResponsiveAppBar />
+            <Outlet/> {/*todo lo que esta anidado dentro de publico */}
         </div>
     )
 }
