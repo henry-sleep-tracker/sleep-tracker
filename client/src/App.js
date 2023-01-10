b6799050b419460eb1591305cfbfd459994b427f
jfparrado <jnsparrado@hotmail.com>
2023-01-10 15:01:35 -0500

    refactor (client//app): se reorganizan las importaciones por orden alfabetico

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,29 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { AuthContextProvider } from "./actions/authContext";
 import axios from "axios";
+
+import ChangePassword from "./components/Profile/ChangePassword.jsx";
+import CommentCreate from "./components/Comments/Comment";
+import ConoceAlEquipo from "./components/Home/ConoceAlEquipo";
+import Dashboard from "./components/dashboard/Dashboard.js";
+import DeleteUser from "./components/Profile/DeleteProfile.jsx";
+import Fitbit from "./components/SignUp/Fitbit";
+import ForgotPassword from "./components/ForgotPassword/ForgotPassword.jsx";
+import GraphWM from "./components/Graph-Week/Graph-W-M.jsx";
 import Home from "./components/Home/Home.jsx";
+import LandingPage from "./components/LandingPage/LandingPage.jsx";
 import LogIn from "./components/LogIn/LogIn";
 import LogOut from "./components/LogOut/LogOut.jsx";
-import Register from "./components/Register/Register";
 import NotFound from "./components/NotFound/NotFound";
-import Fitbit from "./components/SignUp/Fitbit";
-import LandingPage from "./components/LandingPage/LandingPage.jsx";
-import Dashboard from "./components/dashboard/Dashboard.js";
-import Record from "./components/Record/Record.jsx";
-import GraphWM from "./components/Graph-Week/Graph-W-M.jsx";
-import PublicRoute from "./components/PublicRoute/PublicRoute";
-import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
-import { AuthContextProvider } from "./actions/authContext";
 import Pricing from "./components/PlanesPago/PlanesPago.jsx";
-import ConoceAlEquipo from "./components/Home/ConoceAlEquipo";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import Profile from "./components/Profile/Profile.jsx";
-import ChangePassword from "./components/Profile/ChangePassword.jsx";
-import DeleteUser from "./components/Profile/DeleteProfile.jsx";
-import ForgotPassword from "./components/ForgotPassword/ForgotPassword.jsx";
+import PublicRoute from "./components/PublicRoute/PublicRoute";
+import Record from "./components/Record/Record.jsx";
+import Register from "./components/Register/Register";
 import ResetPassword from "./components/ResetPassword/ResetPassword.jsx";
-import CommentCreate from "./components/Comments/Comment";
+
 import "./App.css";
 //The following link must be un-comented on gitHub if you wanna work with your "npm start" running
 axios.defaults.baseURL = "http://localhost:3001/";
