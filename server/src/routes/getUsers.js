bd8907ce2de8d9a989fc6698d3d863e349bec278
Pablo Gomez <pablogomez3360@gmail.com>
2023-01-11 12:15:22 -0300

    Arreglos de visualizacion en Lista de Usuarios

--- server/src/routes/getUsers.js
+++ server/src/routes/getUsers.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const router = Router();
-const { User, Plans } = require("../db.js");
+const { User, Plan } = require("../db.js");
 
 router.get("/", async (req, res) => {
   let { page, limit } = req.query;
@@ -10,10 +10,23 @@ router.get("/", async (req, res) => {
   limit = parseInt(limit);
 
   try {
-    const users = await User.findAll({
+    const usersDbResponse = await User.findAll({
       order: [["lastNames", "ASC"]],
       paranoid: false,
+      raw: true
     });
+
+    const plans = await Plan.findAll({raw: true});
+    
+    const users = usersDbResponse.map( user => {
+      const plan = plans.find( plan => plan.userId === user.id);
+      if(plan){
+        return ({ ...user, plan });
+      } else {
+        return user;
+      }
+    });
+
     res.status(200).send({
       users: users.slice((page - 1) * limit, page * limit),
       total: users.length,
