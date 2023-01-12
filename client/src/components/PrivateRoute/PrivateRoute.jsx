36f304d032859ebc06188d8ceb93358b2a2a55c3
jfparrado <jnsparrado@hotmail.com>
2023-01-12 00:20:17 -0500

    no hay cambios

--- client/src/components/PrivateRoute/PrivateRoute.jsx
+++ client/src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,4 +1,4 @@
-import {Navigate, Outlet} from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom"
 import {useAuthContext} from "../../actions/authContext"
 import ResponsiveAppBar from "../Home/Nav";
 export default function PrivateRoute(){
@@ -6,10 +6,11 @@ export default function PrivateRoute(){
     if (!isAuthenticated) { //si esta autenticado que vaya a la seccion privada
         return <Navigate to="/"/> //aca se quita el return
     }
+    
     return(
         <div>
             <ResponsiveAppBar />
-            <Outlet/> {/*todo lo que esta anidado dentro de publico */}
+            <Outlet /> {/*todo lo que esta anidado dentro de publico */}
         </div>
     )
 }
