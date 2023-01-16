e1b826d0cf3e7be19dc3adbe0d3cfa52f99522c7
elhombrej <94816708+elhombrej@users.noreply.github.com>
2023-01-16 18:20:02 -0300

    deploy v3

--- server/src/db.js
+++ server/src/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, CURRENT_DB } =
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } =
   process.env;
 const sequelize = new Sequelize(
   // `postgresql://postgres:WrpAbk2oBdKzw2PgQQNg@containers-us-west-153.railway.app:7381/railway`,
