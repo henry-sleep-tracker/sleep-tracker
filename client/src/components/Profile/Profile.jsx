207bfa3a2fcc118cc9face491f775bac3065b9a0
Juan Rivera <jjrr0213@gmail.com>
2023-01-15 22:56:07 -0600

    Perfil Fixed

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -7,9 +7,9 @@ import {
   Card,
   CardContent,
   Grid,
-  Link,
   TextField,
   Typography,
+  MenuItem
 } from "@mui/material";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -20,9 +20,13 @@ import EmailIcon from "@mui/icons-material/Email";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import PlaceIcon from "@mui/icons-material/Place";
 import { Helmet } from "react-helmet";
-import IconButton from "@mui/material/IconButton";
-import PhotoCamera from "@mui/icons-material/PhotoCamera";
-import Avatar from "@mui/material/Avatar";
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import Avatar from '@mui/material/Avatar';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import AddCommentIcon from "@mui/icons-material/AddComment";
 import PaymentIcon from "@mui/icons-material/Payment";
 
@@ -30,6 +34,8 @@ const Profile = () => {
   const { createPassword } = useAuthContext();
   const currentUser = useSelector((state) => state.users.currentUser);
   const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
+  const [open2, setOpen2] = useState(false);
   const [editNames, setEditNames] = useState(false);
   const [editEmail, setEditEmail] = useState(false);
   const [editiBirthday, setEditBirthday] = useState(false);
@@ -42,20 +48,26 @@ const Profile = () => {
     birthday: "",
     nationality: "",
   });
+  console.log(inputs);
+  let nationalities = [
+    'Afganistan', 'Albania', 'Alemania', 'Andorra', 'Angola', 'Antigua y Barbuda', 'Arabia Saudita', 'Argelia', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaiyán', 'Bahamas', 'Bangladés', 'Barbados', 'Baréin', 'Bélgica', 'Belice', 'Bielorrusia', 'Benín', 'Birmania', 'Bolivia', 'Bosnia y Herzegovina', 'Botsuana', 'Brasil', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Bután', 'Cabo Verde', 'Camboya', 'Camerún', 'Canadá', 'Catar', 'Chad', 'Chile', 'China', 'Chipre', 'Ciudad del Vaticano', 'Colombia', 'Comoras', 'Corea del Norte', 'Corea del Sur', 'Costa de Marfil', 'Costa Rica', 'Croacia', 'Cuba', 'Dinamarca', 'Dominica', 'Ecuador', 'Egipto', 'El Salvador', 'Emiratos Árabes Unidos', 'Eritrea', 'Eslovaquia', 'Eslovenia', 'España', 'Estados Unidos', 'Estonia', 'Etiopía', 'Filipinas', 'Finlandia', 'Fiyi', 'Francia', 'Gabón', 'Gambia', 'Georgia', 'Ghana', 'Granada', 'Grecia', 'Guatemala', 'Guinea', 'Guinea-Bisáu', 'Guinea Ecuatorial', 'Guyana', 'Haití', 'Honduras', 'Hungría', 'India', 'Indonesia', 'Irak', 'Irán', 'Irlanda', 'Islandia','Islas Marshall', 'Israel', 'Italia', 'Jamaica', 'Japón', 'Jordania', 'Kazajistán', 'Kenia', 'Kirguistán', 'Kiribati', 'Kuwait', 'Laos', 'Lesoto', 'Letonia', 'Líbano', 'Liberia', 'Libia', 'Liechtenstein', 'Lituania', 'Luxemburgo', 'Macedonia del Norte', 'Madagascar', 'Malasia', 'Malaui', 'MalGridas', 'Mali', 'Malta', 'Marruecos',  'Mauricio', 'Mauritania', 'México', 'Micronesia', 'Moldavia', 'Mónaco', 'Mongolia', 'Montenegro', 'Mozambique', 'Namibia', 'Nauru', 'Nepal', 'Nicaragua', 'Níger', 'Nigeria', 'Noruega', 'Nueva Zelanda', 'Omán', 'Países Bajos', 'Pakistán', 'Palaos', 'Palestina', 'Panamá', 'Papúa Nueva Guinea', 'Paraguay', 'Perú', 'Polonia', 'Portugal', 'Reino Unido', 'República Checa','República Centroafricana', 'República del Congo', 'República Democrática del Congo','República Dominicana', 'Ruanda', 'Rumania', 'Rusia', 'Islas Salomón', 'Samoa', 'San Cristóbal y Nieves', 'San Marino', 'San Vicente y las Granadinas', 'Santa Lucía', 'Santo Tomé y Príncipe', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leona', 'Singapur', 'Siria', 'Somalia', 'Sri Lanka', 'Suazilandia', 'Sudáfrica', 'Sudán', 'Sudán del Sur', 'Suecia', 'Suiza', 'Surinam', 'Tailandia', 'Tanzania', 'Tayikistán', 'Timor Oriental', 'Togo', 'Tonga', 'Trinidad y Tobago', 'Túnez', 'Turkmenistán', 'Turquía', 'Tuvalu', 'Ucrania', 'Uganda', 'Uruguay', 'Uzbekistán', 'Vanuatu', 'Venezuela', 'Vietnam', 'Yemen', 'Yibuti', 'Zambia', 'Zimbabue'
+  ];
+  let keyNationalities = 0;
+  let yourDate = new Date();
 
   useEffect(() => {
-    if (currentUser.hashedPassword!== null) {
+    if (currentUser.password !== null) {
       createPassword();
     }
-  }, [currentUser]);
+  }, [currentUser, createPassword]);
 
-  const convertirBase64 = async (e) => {
+  const convertirBase64 = async (e) =>{
     let reader = new FileReader();
-    reader.readAsDataURL(e.target.files[0]);
-    reader.onload = function () {
+     reader.readAsDataURL(e.target.files[0]);
+     reader.onload = function(){
       let base64 = reader.result;
-      setImage(base64);
-    };
+      setImage(base64)
+    }
   };
 
   const handleInputs = (e) => {
@@ -99,10 +111,54 @@ const Profile = () => {
     }
   };
 
+  const handleSelect = (event) => {
+    setInputs({
+      ...inputs,
+      nationality: event.target.value
+    });
+  };
+
+  const handleClickDelete = () => {
+    setOpen(true);
+  };
+
+  const handleClickConfirm = () => {
+    setOpen2(true);
+  };
+
+  const handleNo = () => {
+    setOpen(false);
+  };
+
+  const handleNo2 = () => {
+    setOpen2(false);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleClose2 = () => {
+    setOpen2(false);
+  };
+
+  const handleDelete = async (e) => {
+    e.preventDefault();
+    try {
+      setOpen(false);
+      const eliminar = "";
+      dispatch(updateImage(currentUser.id, eliminar))
+      setImage("");
+    
+    } catch (error) {
+      console.log("el error es:", error);
+    }
+  };
+
   const handleImage = async (e) => {
     e.preventDefault();
     try {
-      dispatch(updateImage(currentUser.id, image));
+      dispatch(updateImage(currentUser.id, image))
       setImage("");
     } catch (error) {
       console.log("el error es:", error);
@@ -112,6 +168,7 @@ const Profile = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+      setOpen2(false);
       dispatch(updateUser(currentUser.id, inputs));
       setInputs({
         names: "",
@@ -135,7 +192,7 @@ const Profile = () => {
       justifyContent="center"
       direction="column"
       alignItems="center"
-      spacing={3}
+      spacing={2}
     >
       <Helmet>
         <title>Perfil | Sleep Tracker</title>
@@ -159,95 +216,66 @@ const Profile = () => {
         </Button>
       </Grid> */}
 
-      <Grid item>
+      <Grid container
+        display= "flex"
+        justifyContent="space-evenly"
+        gap={50}
+      >
+        <Grid item>
         {/* <Link to={`/private/delete-user/${currentUser.id}`}> */}
-        <Button
-          href={`/private/delete-user/${currentUser.id}`}
-          startIcon={<DeleteIcon />}
-          variant="outlined"
-          color="error"
-          id="ButtonDelete"
-        >
-          Borrar usuario
-        </Button>
-        {/* </Link> */}
-      </Grid>
-
-      <Grid item>
-        {/* <Link to={`/private/change-password/${currentUser.id}`}> */}
-        <Button
-          href={`/private/change-password/${currentUser.id}`}
-          startIcon={<PasswordIcon />}
-          variant="outlined"
-          id="ButtonPassword"
-        >
-          Cambiar contraseña
-        </Button>
-        {/* </Link> */}
-      </Grid>
-
-      {/* <Grid
-        item
-      >
-        <Button
-          href='/private/createcomment'
-          startIcon={<AddCommentIcon />}
-          variant='outlined'
-          id='buttonCreatecomment'
-        >
-          Dejar comentario
-        </Button>
-      </Grid>
-
-      <Grid
-        item
-      >
-        <Button
-          href='/private/planes'
-          startIcon={<PaymentIcon />}
-          variant='outlined'
-          id='buttonPlanes'
-        >
-          Planes de pago
-        </Button>
-      </Grid> */}
-
-      {/* <Grid
-        item
-      >
-        <img src="https://upload.wikimedia.org/wikipedia/en/4/4c/Team_8_logo.png" alt="Imagen aqui" />
-      </Grid> */}
-
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        flex={3}
-        p={2}
-      >
-        <Avatar
-          alt="Not found"
-          srcSet={currentUser.image}
-          sx={{ width: 200, height: 200 }}
-        />
-        {!image ? (
-          <IconButton
-            color="primary"
-            aria-label="upload picture"
-            component="label"
+          <Button
+            href={`/private/change-password/${currentUser.id}`}
+            startIcon={<PasswordIcon />}
+            variant="outlined"
+            id="ButtonPassword"
           >
-            <input
-              hidden
-              accept="image/*"
-              type="file"
-              onChange={(e) => convertirBase64(e)}
-            />
-            <PhotoCamera />
-          </IconButton>
-        ) : (
-          <Button onClick={handleImage}>Confirmar</Button>
-        )}
+            Cambiar contraseña
+          </Button>
+        </Grid>
+
+        <Grid item
+        >
+          <Grid container display= "flex" flexDirection= "column" gap={2}>
+          <Button
+            href={`/private/delete-user/${currentUser.id}`}
+            startIcon={<DeleteIcon />}
+            variant="outlined"
+            color="error"
+            id="ButtonDelete"
+          >
+            Borrar usuario
+          </Button>{currentUser.image ? <Button onClick={handleClickDelete}>Eliminar foto</Button>: null}
+          </Grid>
+        </Grid>
+        {/* </Link> */}
+      </Grid>
+
+      <Grid
+        item
+      >
+        <Grid container direction= "column" gap={1} justifyContent= "center">
+          <Avatar 
+            alt = "Not found"
+            srcSet={currentUser.image}
+            sx={{ width: 200, height: 200 }}
+          />
+          {image ? 
+            <Button color="success" size="small" variant="contained" onClick={handleImage}>Confirmar foto</Button> 
+            : 
+            <Button sx={{
+              ml: 1,
+              "&.MuiButtonBase-root:hover": {
+                bgcolor: "transparent"
+              }
+            }} 
+            startIcon = {<PhotoCamera/>} 
+            color="primary" 
+            aria-label="upload picture" 
+            component="label">
+              <input hidden accept="image/*" type="file" onChange = {(e) => convertirBase64(e)}/>
+            </Button>}
+         </Grid>
+
       </Grid>
 
       <Grid item>
@@ -334,9 +362,11 @@ const Profile = () => {
                   <Typography variant="h6">{currentUser.birthday}</Typography>
                 ) : (
                   <TextField
-                    type="text"
+                    helperText="Fecha de nacimiento"
+                    type="date"
                     name="birthday"
-                    label="Nueva fecha de nacimiento"
+                    min="1900-01-01" max={yourDate}
+                    placeholder=""
                     value={inputs.birthday}
                     onChange={handleInputs}
                   />
@@ -366,12 +396,22 @@ const Profile = () => {
                   </Typography>
                 ) : (
                   <TextField
-                    type="text"
-                    name="nationality"
-                    label="Nueva nacionalidad"
+                  required
+                  select
+                  label="Nacionalidad"
+                  defaultValue=""
+                  helperText="Seleccione su nacionalidad"
                     value={inputs.nationality}
-                    onChange={handleInputs}
-                  />
+                    onChange={handleSelect}
+                  >
+                     {nationalities?.map((nationality) => (
+                    <MenuItem key={keyNationalities++}
+                      value={nationality}
+                    >
+                      {nationality}
+                    </MenuItem>
+                  ))}
+                  </TextField>
                 )}
               </Grid>
 
@@ -390,19 +430,20 @@ const Profile = () => {
               <Grid item>
                 <Typography variant="h5">Plan actual:</Typography>
               </Grid>
-              <br />
-
+              <Grid item>
               <Button variant="contained" href="/private/planes">
-                {currentUser.plan?.name}
+                  {currentUser.plan?.name}
               </Button>
-
+              </Grid>
+              
+      
               <Grid item>
                 {inputs.names ||
                 inputs.email ||
                 inputs.birthday ||
                 inputs.nationality ? (
                   <Button
-                    onClick={(e) => handleSubmit(e)}
+                    onClick={handleClickConfirm}
                     color="success"
                     variant="contained"
                     type="submit"
@@ -417,6 +458,51 @@ const Profile = () => {
           </CardContent>
         </Card>
       </Grid>
+
+      <Grid item >
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"¿ Desea eliminar la imagen de perfil ?"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Si continuas tu foto de perfil será eliminada para siempre.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={(e) => handleDelete(e)} autoFocus>Si, deseo eliminarla</Button>
+            <Button onClick={handleNo}>Cancelar</Button> 
+          </DialogActions>
+        </Dialog>
+    </Grid>
+      
+      <Grid item >
+          <Dialog
+            open={open2}
+            onClose={handleClose2}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">
+              {"¿ Desea realizar los cambios en tu perfil?"}
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                Si continuas tus datos se modificaran con la nueva información que has colocado.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={(e) => handleSubmit(e)} autoFocus>Si, quiero realizar los cambios</Button>
+              <Button onClick={handleNo2}>Cancelar</Button>
+            </DialogActions>
+          </Dialog>
+      </Grid>
+
     </Grid>
   );
 };
