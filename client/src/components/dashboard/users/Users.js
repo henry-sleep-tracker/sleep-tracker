638c76e9633545532f70e179b75e7a74b1830aa2
jfparrado <jnsparrado@hotmail.com>
2023-01-19 16:41:21 -0500

    feat(client/components/dashboard/users): Lo ismo pero estilisado con prettier

--- client/src/components/dashboard/users/Users.js
+++ client/src/components/dashboard/users/Users.js
@@ -1,23 +1,23 @@
 import { useEffect, useMemo, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { styled, Box, Typography } from "@mui/material";
-import { 
-  DataGrid, 
-  GridToolbarContainer, 
-  GridToolbarColumnsButton, 
-  gridClasses, 
-  esES, 
+import {
+  DataGrid,
+  GridToolbarContainer,
+  GridToolbarColumnsButton,
+  gridClasses,
+  esES,
   gridPageCountSelector,
   gridPageSelector,
   useGridApiContext,
-  useGridSelector
+  useGridSelector,
 } from "@mui/x-data-grid";
 import { grey } from "@mui/material/colors";
-import Pagination from '@mui/material/Pagination';
+import Pagination from "@mui/material/Pagination";
 
 import { getUsers } from "../../../actions/users";
 import UsersActions from "./UsersActions";
-import GridToolbarFilters from './GridToolbarFilters'
+import GridToolbarFilters from "./GridToolbarFilters";
 
 const DrawerHeader = styled("div")(({ theme }) => ({
   display: "flex",
@@ -28,7 +28,7 @@ const DrawerHeader = styled("div")(({ theme }) => ({
   ...theme.mixins.toolbar,
 }));
 
-function CustomToolbar( { filters, setFilters }) {
+function CustomToolbar({ filters, setFilters }) {
   return (
     <GridToolbarContainer sx={{ mr: 2 }}>
       <GridToolbarColumnsButton />
@@ -43,37 +43,40 @@ function CustomPagination({ totalUsers }) {
   const pageCount = useGridSelector(apiRef, gridPageCountSelector);
 
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-      <Box>
-        Total de Usuarios: { totalUsers }
-      </Box>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <Box>Total de Usuarios: {totalUsers}</Box>
       <Pagination
         color="primary"
         count={pageCount}
         page={page + 1}
         onChange={(event, value) => apiRef.current.setPage(value - 1)}
       />
-  </Box>
+    </Box>
   );
-};
+}
 
 function UsersContent() {
-
   const dispatch = useDispatch();
-  const users = useSelector(state => state.users.users);
-  
+  const users = useSelector((state) => state.users.users);
+
   const [rowId, setRowId] = useState(null);
   const [pageState, setPageState] = useState({
     isLoading: false,
     users: users.users,
     total: users.total,
     page: 1,
-    pageSize: 5
+    pageSize: 5,
   });
 
   const [filters, setFilters] = useState({
-    nationality: '',
-    plan: ''
+    nationality: "",
+    plan: "",
   });
 
   useEffect(() => {
@@ -82,21 +85,26 @@ function UsersContent() {
   }, []);
 
   useEffect(() => {
-    setPageState(old => ({ ...old, isLoading: false, users: users.users, total: users.total }));
+    setPageState((old) => ({
+      ...old,
+      isLoading: false,
+      users: users.users,
+      total: users.total,
+    }));
   }, [users]);
 
   useEffect(() => {
-      setPageState(old => ({ ...old, isLoading: true }));
-      dispatch(getUsers(pageState.page, pageState.pageSize, filters));
-      // eslint-disable-next-line
+    setPageState((old) => ({ ...old, isLoading: true }));
+    dispatch(getUsers(pageState.page, pageState.pageSize, filters));
+    // eslint-disable-next-line
   }, [pageState.page, pageState.pageSize]);
 
   useEffect(() => {
-    setPageState(old => ({ ...old, isLoading: true }));
+    setPageState((old) => ({ ...old, isLoading: true }));
     dispatch(getUsers(pageState.page, pageState.pageSize, filters));
     // eslint-disable-next-line
   }, [filters]);
-  
+
   // useEffect(() => {
   //   console.log('users', users);
   //   console.log('pageState', pageState);
@@ -108,29 +116,77 @@ function UsersContent() {
 
   const columns = useMemo(
     () => [
-      { field: "fullName", headerName: "Nombre Completo", width: 200, valueGetter: getFullName },
-      { field: "lastNames", headerName: "Apellidos", width: 170, editable: true },
+      {
+        field: "fullName",
+        headerName: "Nombre Completo",
+        width: 200,
+        valueGetter: getFullName,
+      },
+      {
+        field: "lastNames",
+        headerName: "Apellidos",
+        width: 170,
+        editable: true,
+      },
       { field: "names", headerName: "Nombres", width: 170, editable: true },
       { field: "email", headerName: "Email", width: 170 },
-      { field: "plan", headerName: "Suscripción", width: 125, headerAlign: 'center', align: 'center',
-          valueGetter:  params => { 
-          return params.row.plan?.name || 'Basico';
-        }
+      {
+        field: "plan",
+        headerName: "Suscripción",
+        width: 125,
+        headerAlign: "center",
+        align: "center",
+        valueGetter: (params) => {
+          return params.row.plan?.name || "Basico";
+        },
       },
-      { field: "isAdmin", headerName: "Admin", width: 85, headerAlign: 'center', align: 'center', 
-        type: "singleSelect", editable: true, 
-        valueOptions: [{ value: true, label: 'Si' }, { value: false, label: 'No'} ], 
-        valueFormatter: params => {
-          if(params.value === true ) return 'Si';
-          else return 'No';
-        }
+      {
+        field: "isAdmin",
+        headerName: "Admin",
+        width: 85,
+        headerAlign: "center",
+        align: "center",
+        type: "singleSelect",
+        editable: true,
+        valueOptions: [
+          { value: true, label: "Si" },
+          { value: false, label: "No" },
+        ],
+        valueFormatter: (params) => {
+          if (params.value === true) return "Si";
+          else return "No";
+        },
       },
-      { field: "birthday", headerName: "Nacimiento", type: "date", editable: true },
-      { field: "nationality", headerName: "Nacionalidad", width: 125, editable: true },
-      { field: "lastConnection", headerName: "Última Conexión", type: "date", width: 125 }, 
-      { field: "createdAt", headerName: "Creado", type: "date", width: 175, editable: true },
+      {
+        field: "birthday",
+        headerName: "Nacimiento",
+        type: "date",
+        editable: true,
+      },
+      {
+        field: "nationality",
+        headerName: "Nacionalidad",
+        width: 125,
+        editable: true,
+      },
+      {
+        field: "lastConnection",
+        headerName: "Última Conexión",
+        type: "date",
+        width: 125,
+      },
+      {
+        field: "createdAt",
+        headerName: "Creado",
+        type: "date",
+        width: 175,
+        editable: true,
+      },
       { field: "deletedAt", headerName: "Eliminado", type: "date", width: 175 },
-      { field: "actions", headerName: "Acciones", type: "actions",
+      {
+        field: "actions",
+        headerName: "Acciones",
+        type: "actions",
         renderCell: (params) => (
           <UsersActions {...{ params, rowId, setRowId, pageState }} />
         ),
@@ -164,15 +220,14 @@ function UsersContent() {
           }}
           columns={columns}
           rows={pageState.users}
-
           pageSize={pageState.pageSize}
-
           paginationMode="server"
           rowCount={pageState.total}
           loading={pageState.isLoading}
-          onPageChange={ (newPage) => setPageState( old => ({ ...old, page: newPage + 1}) ) }
+          onPageChange={(newPage) =>
+            setPageState((old) => ({ ...old, page: newPage + 1 }))
+          }
           page={pageState.page - 1}
-
           getRowSpacing={(params) => ({
             top: params.isFirstVisible ? 0 : 3,
             bottom: params.isLastVisible ? 0 : 3,
@@ -184,7 +239,7 @@ function UsersContent() {
             },
           }}
           onCellEditCommit={(params) => setRowId(params.id)}
-          components={{ 
+          components={{
             Toolbar: CustomToolbar,
             Pagination: CustomPagination,
           }}
@@ -192,7 +247,6 @@ function UsersContent() {
             pagination: { totalUsers: users.total },
             toolbar: { filters: filters, setFilters: setFilters },
           }}
-
         />
       </Box>
 
